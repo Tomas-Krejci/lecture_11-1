0199b962ca7ba9058564987c15c7631351aa3288
Tomas-Krejci <226398@vutbr.cz>
2022-05-03 09:43:20 +0200

    first

--- fibo.py
+++ fibo.py
@@ -1,7 +1,16 @@
+l = 10
+def recursive_nth_fibo(n):
+    if n == 0:
+        return n
+
+    elif n == 1:
+        return n
+
+    else:
+        return recursive_nth_fibo(n-1) + recursive_nth_fibo(n-2)
 
 def main():
-    pass
-
+    print(recursive_nth_fibo(l))
 
 if __name__ == "__main__":
     main()
